d65b49cf3a606c2cd9fcf0a611b1016006b1fcf4
OscarCMZ <cmzatUCL@gmail.com>
2023-01-07 00:12:25 +0000

    add testbench file

---
+++ single core/XRISC_single_tb.sv
@@ -0,0 +1,34 @@
+module XRISC_single_tb();
+
+    logic           clk;
+    logic           reset;
+    logic   [31:0]  WriteData, DataAdr;
+    logic           MemWrite;
+
+//initialize test
+initial 
+    begin
+        reset <= 1;#22; reset <= 0;
+    end
+
+//generate clock to sequence tests
+always
+    begin
+        clk <= 1; #5; clk <=0; #5;
+    end
+
+//check results
+always@(negedge clk)
+    begin
+        if(MemWrite) begin
+            if (DataAdr === 100 & WriteData === 25) begin
+                $display("Simulation succeeded");
+                $stop;
+            end else if (DataAdr !== 96) begin
+                $display("Simulation failed");
+                $stop;
+            end
+        end
+    end
+endmodule
+
